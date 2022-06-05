cf0b35e95fe9d144ff8800e1d7f12d606bb46341
Jorge-Ortiz-M <jorge.ortiz@icalialabs.com>
2022-06-05 12:05:00 -0500

    Adding the loadsh module and mongoose as the database locally.

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ const express = require("express"); // Require the EXPRESS module.
 const bodyParser = require("body-parser");  // Require the Body Parser module.
 const ejs = require("ejs");     // Require the EJS module. 
 const { getDate } = require("./javascript/date");
+const mongoose = require("mongoose");   // Connecting to our local mongo database.
+const _ = require('lodash');    // Adding the Lodash module.
+// ---------------------------------------
+// ------- Creating and connecing to the DB. ------------
+mongoose.connect("mongodb://localhost:27017/DailyDB", { useNewUrlParser: true });
 // ---------------------------------------
 // --------- Import Local module ----------
 const date = require(__dirname + '/javascript/date');
@@ -12,16 +17,44 @@ const date = require(__dirname + '/javascript/date');
 // ----------- Local Variables -----------
 const port = 9000;      // The port where is going to listen.
 const app = express();  // Adding express to app in order to syncronisc each webpage.
-const notesList = [];
 // ---------------------------------------
 // -------- Modules configuration. ---------
 app.use(bodyParser.urlencoded({extended: true}));   // Configuration in order to get data from the forms.
 app.use(express.static("public"));  // Configuration in order to call local files inside of the public folder.
 app.set('view engine', 'ejs');  // Configuration in order to use EJS in our web application.
 // ---------------------------------------
+// ------------ Create DB Schemas ----------
+const noteSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    date:{
+        type: String,
+        required: true
+    }
+});
+// ---------------------------------------
+// ------------ Create Models. --------------
+const Note = mongoose.model("Note", noteSchema);
+// ---------------------------------------
+// --- Create Simple Objects for test. ---
+//const note1 = new Note({
+//    title: "My morning routine",
+//    description: "The first argument is the singular name of the collection your model is for. Mongoose automatically looks for the plural, lowercased version of your model name. Thus, for the example above, the model Tank is for the tanks collection in the database.",
+//    date: date.getDate()
+//});
+//note1.save()
+// ---------------------------------------
 // ------------ GET request --------------
 app.get("/", (req, res) => {    // Route of the main application.
-    res.render('pages/index', {current_date: date.getDate(), notes: notesList});
+    Note.find({}, function(err, notesList){
+        res.render('pages/index', {current_date: date.getDate(), notes: notesList});
+    });
 });
 
 app.get("/about", (req, res) => {   // About page from the application.
@@ -36,12 +69,11 @@ app.get('/notes/:noteID', (req, res) => {       // Show page where render a note
     const id = req.params.noteID;
     const note = notesList[id];
     res.render('pages/show', {current_date: date.getDate(), note: note});
-  })
+  });
 // ---------------------------------------
 // ------------ POST request -------------
 app.post("/new", (req, res) => {
     const note = {title: req.body.title, description: req.body.description, date: date.getDate()};
-    notesList.push(note);
     res.redirect("/");
 });
 // ---------------------------------------
